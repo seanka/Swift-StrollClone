a7d4e1e2b5f2ab20856097704cb7ee2b11ced5b5
seanka <and.seanka@gmail.com>
2025-09-16 08:45:34 +0700

    Initial Commit

---
+++ Stroll/StrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrollApp.swift
+//  Stroll
+//
+//  Created by Sean Anderson on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct StrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
